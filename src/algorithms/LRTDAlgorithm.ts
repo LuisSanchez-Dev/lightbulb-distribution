c52acbda2ee71bb0f38862fa67c7018d1965797e
Luis Sanchez <luis.sanchez.dev@hotmail.com>
2022-01-06 19:16:53 -0600

    Add initial LRTD algorithm

--- src/algorithms/LRTDAlgorithm.ts
+++ src/algorithms/LRTDAlgorithm.ts
@@ -1,21 +1,90 @@
 import BaseAlgorithm from "./BaseAlgorithm";
 
+class Square {
+  isWall = false;
+  isIlluminated = false;
+  isLightbulb = false;
+  constructor(isWall: boolean) {
+    this.isWall = isWall;
+  }
+
+  static toStringArrays(squares: Square[][]): string[][] {
+    return squares.map((row) => {
+      return row.map((square) => {
+        if (square.isWall) {
+          return "1";
+        } else if (square.isIlluminated && !square.isLightbulb) {
+          return "0";
+        } else if (square.isLightbulb) {
+          return "L";
+        } else {
+          return "-";
+        }
+      });
+    });
+  }
+}
+
 export default class LRTD extends BaseAlgorithm {
   name = "LRTD";
   description =
     "Left-Right Top-Down algorithm, it goes from the upper left corner to the lower right corner, placing lightbulbs every time there is no light in that position";
   declare input: string[][];
-  run(): string[][] {
-    console.log("LRTD input:", this.input);
-    return [
-      ["L", "0", "0", "0"],
-      ["0", "L", "0", "0"],
-      ["0", "0", "L", "0"],
-      ["0", "0", "0", "L"],
-    ];
-  }
 
   constructor(input: string[][]) {
     super(input);
+  }
+
+  run(): string[][] {
+    const squares = this.input.map((row) => {
+      return row.map((value) => {
+        return new Square(value === "1");
+      });
+    });
+    console.log(squares);
+    for (let i = 0; i < squares.length; i++) {
+      const row = squares[i];
+      for (let j = 0; j < row.length; j++) {
+        const square = row[j];
+        if (!square.isWall && !square.isIlluminated) {
+          square.isLightbulb = square.isIlluminated = true;
+          let offsetX = 1;
+          if (j + offsetX < row.length) {
+            let nextSquare: Square = row[j + offsetX];
+            while (!nextSquare.isWall) {
+              nextSquare.isIlluminated = true;
+              offsetX++;
+              if (j + offsetX >= row.length) {
+                break;
+              }
+              nextSquare = row[j + offsetX];
+            }
+          }
+          let offsetY = 1;
+          if (i + offsetY < squares.length) {
+            let nextSquare: Square = squares[i + offsetY][j];
+            console.log({ i, j });
+            while (nextSquare != undefined && !nextSquare.isWall) {
+              console.log("Starting column loop");
+              console.log({ i: i + offsetY, j, nextSquare });
+              nextSquare.isIlluminated = true;
+              offsetY++;
+              console.log("Before error");
+              if (i + offsetY >= squares.length) {
+                console.log("After error");
+                break;
+              }
+              nextSquare = squares[i + offsetY][j];
+              console.log({ i: i + offsetY, j, nextSquare });
+              console.log("Ended column loop");
+            }
+          }
+
+          console.log("Ended all collumn");
+        }
+      }
+    }
+    console.log("Converting to strings");
+    return Square.toStringArrays(squares);
   }
 }
--- src/store/modules/distribution.ts
+++ src/store/modules/distribution.ts
@@ -12,9 +12,15 @@ class Distribution extends VuexModule {
   public setOutput(output: string[][]): void {
     this.output = output;
   }
-  @Action
+  @Action({ rawError: true })
   public updateInput(input: string): void {
-    const lrtd = new LRTDAlgorithm([["0", "1"]]);
+    const lrtd = new LRTDAlgorithm([
+      ["0", "0", "1", "0", "0"],
+      ["0", "0", "1", "0", "0"],
+      ["1", "1", "1", "1", "1"],
+      ["0", "0", "1", "0", "0"],
+      ["0", "0", "1", "0", "0"],
+    ]);
     this.context.commit("setOutput", lrtd.run());
   }
 }
